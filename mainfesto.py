983caf91586a769c404286f7179e280b09f4bc02
Shaohua Yang <lnhn@live.cn>
2026-01-22 11:33:17 +0800

    Create mainfesto.py

---
+++ mainfesto.py
@@ -0,0 +1,13 @@
+# ZenFlux 启动脚本  -2026.01.21
+
+import datetime 
+
+def start_new_life():
+  status = "Action" #不再是 "Thinking"
+  identify = "Maker" #不再是"Student"
+  print(f"[{datetime.datetime.now()}]告别985经营幻想，欢迎来到真实世界。“)
+  print("今日目标：不求通透，只求一行代码运行成功。")
+
+if __name__ == "__main__":
+  start_new_lift()
+
