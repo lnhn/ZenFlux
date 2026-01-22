d76f7db993aeedab0286b2a15dfaac47cd2cbe31
Shaohua Yang <lnhn@live.cn>
2026-01-22 11:43:43 +0800

    Update mainfesto.py

--- mainfesto.py
+++ mainfesto.py
@@ -5,7 +5,7 @@ import datetime
 def start_new_life():
     status = "Action" #不再是 "Thinking"
     identify = "Maker" #不再是"Student"
-    print(f"[{datetime.datetime.now()}]告别985经营幻想，欢迎来到真实世界。“)
+    print(f"[{datetime.datetime.now()}]告别985精英幻想，欢迎来到真实世界。”)
     print("今日目标：不求通透，只求一行代码运行成功。")
 
 if __name__ == "__main__":
