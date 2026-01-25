44635d63e74d2e528935c7953e8b36079c0c9feb
lnhn <lnhn@live.cn>
2026-01-25 22:38:14 +0800

    update signal logic

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def generate_signal(data):
     data["signal"] = 0
     # Go long when short MA is above long MA
     data.loc[data["ma_short"] > data["ma_long"], "signal"] = 1
-
+    #data.loc[data["price"] >80, "signal"] = 1
     return data["signal"]
 
 def backtest(data, signal):
@@ -41,16 +41,36 @@ def backtest(data, signal):
 
 
 def evaluate(result):
+     #final return of the data 最终的收益
      total_return = result["cumulative_return"].iloc[-1] - 1
+
      print(f"Total Strategy Return: {total_return:.2%}")
+     mdd = max_drawdown(result["cumulative_return"])
+     print(f"Max Drawdown: {mdd:.2%}")
+     annual_return  = annualized_return(result["cumulative_return"])
+     print (f"Annualized Return: {annual_return:.2%}")
      result["cumulative_return"].plot(title="Cumulative Strategy Return")
+     
      plt.show()   
+
+def max_drawdown(cumulative_returns):
+    peak = cumulative_returns.cummax()
+    drawdown = (cumulative_returns - peak) / peak
+    max_dd = drawdown.min()
+    return max_dd
+
+def annualized_return(cumu):
+    n_days = len(cumu)
+    final_value = cumu.iloc[-1]
+    return final_value ** (252 / n_days) - 1
+
     
 
 if __name__ == "__main__":
     data = load_data()
     signal = generate_signal(data)
     result = backtest(data, signal)
+   # plt.plot(data['price'])
     evaluate(result)
 
     
